1d48354f979e6737496ac5de7ffb7794bba3cc48
selu91 <cristea.marius.alexandru@gmail.com>
2016-05-16 18:32:25 +0300

    Fixed compatibility with bundle

--- wppr-custom-icon.php
+++ wppr-custom-icon.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WPPR Custom Icon
 Description: This add-on add a custom icon functionality to your WP Product Review plugin
-Version: 1.0.5
+Version: 1.1.0
 Author: Themeisle
 Author URI:  https://themeisle.com/
 Plugin URI: https://themeisle.com/plugins/WPPR-custom-icon/
@@ -15,9 +15,12 @@ Text Domain: cwppos
 Domain Path: /languages
 */
 
+define("WPPR_CUSTOM_ICON_VERSION",'1.1.0');
+
+
 function wppr_ci_custom_bar_icon() {
 
-    wp_enqueue_script("cwp-custom-bar-icon", plugins_url('custom-bar-icon.js', __FILE__), false, "1.0", "all");
+    wp_enqueue_script("cwp-custom-bar-icon", plugins_url('custom-bar-icon.js', __FILE__), false, WPPR_CUSTOM_ICON_VERSION, "all");
      wp_enqueue_style("font-awesome-cdn", "//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css");
 
 }
